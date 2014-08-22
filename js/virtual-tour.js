a27713253829b111465009e8738182a929a75963
Jackson <jbierfeldt@gmail.com>
2014-08-22 15:35:23 -0500

    Refactoring Code into Module Pattern

--- js/virtual-tour.js
+++ js/virtual-tour.js
@@ -1,12 +1,30 @@
 // Building BF_QUIZ Module according to the principles outlined here: http://yuiblog.com/blog/2007/06/12/module-pattern/
 var VT = {};
 
+VT.settings = function () {
+	return {
+		USER_IS_MOBILE: false,
+		tour_json_path: "http://sandbox.bierfeldt.me/vt/json/tour_path.json",
+		stops_json_path: "http://sandbox.bierfeldt.me/vt/json/tour_stops2.json",
+		init: function() {
+			if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            	this.USER_IS_MOBILE = true;
+        	}
+			console.log("Settings for Virtual Tour");
+			alert(this.USER_IS_MOBILE);
+		}
+	};
+}();
+
 VT.loader = function () {
 	return {
 		init: function() {
+			VT.settings.init();
 			VT.mapManager.init();
 			VT.markerManager.init();
 			VT.locationServices.init();
+			VT.tourManager.init();
+			VT.stopManager.init();
 		}
 	};
 }();
@@ -20,7 +38,7 @@ VT.markerManager = function () {
 	icons = {
 		start: new google.maps.MarkerImage(
 		// URL
-		'../images/start.png',
+		'images/start.png',
 			// (width,height)
 			new google.maps.Size( 18, 32 ),
 			// The origin point (x,y)
@@ -30,7 +48,7 @@ VT.markerManager = function () {
 		),
 		end: new google.maps.MarkerImage(
 			// URL
-			'../images/end.png',
+			'images/end.png',
 			// (width,height)
 			new google.maps.Size( 18, 32 ),
 			// The origin point (x,y)
@@ -40,7 +58,7 @@ VT.markerManager = function () {
 		),
 		unselected: new google.maps.MarkerImage(
 			// URL
-			'../images/unselected_marker.png',
+			'images/unselected_marker.png',
 			// (width,height)
 			new google.maps.Size( 36, 63 ),
 			// The origin point (x,y)
@@ -50,7 +68,7 @@ VT.markerManager = function () {
 		),
 		selected: new google.maps.MarkerImage(
 			// URL
-			'../images/selected_marker.png',
+			'images/selected_marker.png',
 			// (width,height)
 			new google.maps.Size( 50, 87 ),
 			// The origin point (x,y)
@@ -60,7 +78,7 @@ VT.markerManager = function () {
 		),
 		current: new google.maps.MarkerImage(
 			// URL
-			'../images/current_marker.png',
+			'images/current_marker.png',
 			// (width,height)
 			new google.maps.Size( 22, 22 ),
 			// The origin point (x,y)
@@ -71,15 +89,36 @@ VT.markerManager = function () {
 	};
 
 	return {
-		makeMarker: function(position, icon, title) {
-			var marker;
+		markers: new Array(),
+		makeMarker: function(position, icon_kind, title) {
+			var marker, icon_selection;
+			icon_selection = icons[icon_kind];
 			marker = new google.maps.Marker({
 				position: position,
 				map: VT.mapManager.map,
-				icon: icon,
+				icon: icon_selection,
 				title: title
 			});
+			this.markers.push(marker);
 			return marker;
+		},
+		setMarkerPosition: function (marker, position) {
+			marker.setPosition(position);
+		},
+		setMap: function (map) {
+			for (var i = 0; i < this.markers.length; i++) {
+				this.markers[i].setMap(map);
+			}
+		},
+		showMarkers: function () {
+			this.setMap(VT.mapManager.map);
+		},
+		clearMarkers: function () {
+			this.setMap(null);
+		},
+		removeAllMarkers: function () {
+			this.clearMarkers();
+			this.markers = new Array();
 		},
 		init: function() {
 			console.log("loaded markerManager");
@@ -90,7 +129,41 @@ VT.markerManager = function () {
 VT.locationServices = function () {
 	"use strict";
 
-	var watchCurrentLocation;
+	var setCurrentLocationMarker, watchCurrentLocation, displayAndWatchCurrentLocation,
+	ne_bound, sw_bound, bounds, geo_options;
+
+	ne_bound = new google.maps.LatLng(41.799562,-87.587342);
+	sw_bound = new google.maps.LatLng(41.780332,-87.605882);
+	bounds = new google.maps.LatLngBounds(sw_bound, ne_bound);
+
+	geo_options = {
+		enableHighAccuracy: true, 
+		maximumAge        : 3, 
+	};
+
+	setCurrentLocationMarker = function setCurrentLocationMarker(pos) {
+		var user_location, currentLocationMarker;
+		// Get User's location from "pos" callback passed from geolocation
+		VT.locationServices.current_location = new google.maps.LatLng(
+			pos.coords.latitude,
+			pos.coords.longitude
+		);
+		// Set the User's Current Location and Make a Marker
+		VT.locationServices.current_location_marker = VT.markerManager.makeMarker(
+			VT.locationServices.current_location, "current", "Current Location");
+	};
+
+	displayAndWatchCurrentLocation = function displayAndWatchCurrentLocation(position) {
+		if (bounds.contains(new google.maps.LatLng(position.coords.latitude,
+				position.coords.longitude)) == true)
+		{
+			setCurrentLocationMarker(position);
+			// watchCurrentLocation();
+			console.log("tracking location");
+		} else {
+			console.log("not tracking location (outside of bounds)");
+		}
+	};
 
 	watchCurrentLocation = function watchCurrentLocation() {
 		var positionTimer, me;
@@ -109,11 +182,138 @@ VT.locationServices = function () {
 	};
 
 	return {
+		current_location: null,
+		current_location_marker: null,
         init: function() {
+        	// if (USER_IS_MOBILE) {
+         //        if (navigator.geolocation) {
+         //        	navigator.geolocation.getCurrentPosition(displayAndWatchCurrentLocation, null, geo_options);
+         //        } else {
+         //        	return
+         //        }
+         //    }
+         	if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(displayAndWatchCurrentLocation,
+                		null, geo_options);
+            }
         	console.log("loaded locationServices");
         }
     };
 
+}();
+
+VT.tourManager = function ($) {
+	var loadTourPathJSON, loadTourPath;
+
+	// Asynchronously Load the Tour Path Data from its JSON file
+	loadTourPathJSON = function (json_file) {
+		var tour_json_data, request;
+		request = new XMLHttpRequest();
+        request.open("GET", json_file, true);
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                // After loading, set the Tour Data to the parsed JSON data
+                tour_json_data = JSON.parse(request.responseText);
+                for (var i = 0, length = tour_json_data.length; i < length; i++) {
+					VT.tourManager.path_points.push(new google.maps.LatLng(
+						tour_json_data[i].lat, tour_json_data[i].lng
+					));
+				}
+				// Load the path and set it initially
+				loadTourPath().setMap(VT.mapManager.map);
+            } else {
+                alert("Tour JSON error.")
+            }
+        };
+        request.onerror = function() {
+            alert("Tour JSON server connection error.")
+        };
+        request.send();
+	};
+
+	// function to create the polygon path from the waypoints data
+	loadTourPath = function () {
+		var lineSymbol, tourPath;
+		lineSymbol = {
+			path: 'M 0,-1 0,1',
+			strokeOpacity: .5,
+			scale: 3
+		};
+		VT.tourManager.tour_path = new google.maps.Polyline({
+			path: VT.tourManager.path_points,
+			strokeOpacity: 0,
+			icons: [{
+				icon: lineSymbol,
+				offset: '0',
+				repeat: '10px'
+			}],
+			map: VT.mapManager.map
+		});
+		return VT.tourManager.tour_path;
+	};
+
+	return {
+		path_points: new Array(),
+		tour_path: null,
+		showTourPath: function () {
+			this.tour_path.setMap(VT.mapManager.map);
+		},
+		hideTourPath: function () {
+			this.tour_path.setMap(null);
+		},
+		init: function() {
+			loadTourPathJSON(VT.settings.tour_json_path);
+			console.log("loaded tourManager");
+		}
+	};
+}();
+
+VT.stopManager = function ($) {
+	var loadStopJSON, loadStopMarkers;
+
+	// Asynchronously Load the Tour Path Data from its JSON file
+	loadStopJSON = function (json_file) {
+		var stop_json_data, request;
+		request = new XMLHttpRequest();
+        request.open("GET", json_file, true);
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                // After loading, set the Tour Data to the parsed JSON data
+                stop_json_data = JSON.parse(request.responseText);
+                loadStopMarkers(stop_json_data);
+            } else {
+                alert("Tour JSON error.")
+            }
+        };
+        request.onerror = function() {
+            alert("Tour JSON server connection error.")
+        };
+        request.send();
+	};
+
+	// function to create the polygon path from the waypoints data
+	loadStopMarkers = function (stop_data) {
+		var position, marker;
+		for (var i = 0, length = stop_data.length; i < length; i++) {
+			position = new google.maps.LatLng(stop_data[i].marker.lat,
+					stop_data[i].marker.lng);
+			marker = VT.markerManager.makeMarker(position, "unselected",
+					stop_data[i].marker.title);
+			marker.html = "<span class='info-title'>"+stop_data[i].marker.title+
+					"</span><br /><span class='info-description'>"+
+					stop_data[i].marker.description+"</span>";
+			// Add each marker to the stop_markers array
+			VT.stopManager.stop_markers.push(marker);
+		}
+	};
+
+	return {
+		stop_markers: new Array(),
+		init: function() {
+			loadStopJSON(VT.settings.stops_json_path);
+			console.log("loaded stopManager");
+		}
+	};
 }();
 
 VT.mapManager = function () {
@@ -223,9 +423,14 @@ VT.mapManager = function () {
         name: 'Custom Style'
     };
     
-    
     return {
     	map: null,
+    	increaseZoom: function () {
+    		this.map.setZoom(this.map.getZoom()+1);
+    	},
+    	decreaseZoom: function () {
+    		this.map.setZoom(this.map.getZoom()-1);
+    	},
         init: function() {
         	this.map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
@@ -235,4 +440,3 @@ VT.mapManager = function () {
         }
     };
 }();
-
